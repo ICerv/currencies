332edf6b9ca8b3b163577c28c7b28adbb70ac31c
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-21 14:04:36 +0200

    Task completed

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const currencies = {
@@ -7,12 +7,29 @@ const currencies = {
   GBP: { CZK: 29.067 }
 };
 
-const Rate = () => {
+const Rate = ({ from }) => {
+
+  const [rate, setRate] = useState(currencies[from].CZK)
+
+  useEffect(() => {
+    const fetchRate = () => {
+      fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+        .then((response) => response.json())
+        .then((data) => {
+          const rate = data.rates.CZK;
+          setRate(rate);
+        })
+    };
+
+    fetchRate();
+  }, [from]);
+
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
